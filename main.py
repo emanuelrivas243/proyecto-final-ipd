16f6d37aaf114e6fba95dde740c453c30b5563ee
emanuelrivas243 <emanuel.rivas@correounivalle.edu.co>
2025-07-31 13:46:32 -0500

    frontend mejorado y ray implementado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from sklearn.datasets import load_iris
 from sklearn.naive_bayes import GaussianNB
 from pydantic import BaseModel
-import numpy as np  # Importamos numpy
+import numpy as np
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
@@ -17,32 +17,37 @@ class request_body(BaseModel):
     petal_length: float
     petal_width: float
 
-# Cargar dataset
-iris = load_iris()
-X = iris['data']  # type: ignore
-Y = iris['target']  # type: ignore
-target_names = iris['target_names']  # type: ignore
+# Actor de Ray que contiene el modelo y hace predicciones
+@ray.remote
+class IrisModelActor:
+    def __init__(self):
+        iris = load_iris()
+        self.X = iris['data'] # type: ignore
+        self.Y = iris['target'] # type: ignore
+        self.target_names = iris['target_names'] # type: ignore
+        self.model = GaussianNB()
+        self.model.fit(self.X, self.Y)
 
-# Entrenar modelo
-clf = GaussianNB()
-clf.fit(X, Y)
+    def predict(self, data):
+        test_data = np.array([data])
+        class_idx = self.model.predict(test_data)[0]
+        return self.target_names[class_idx]
+
+# Crear instancia del actor
+iris_model_actor = IrisModelActor.remote()
 
 @app.post("/predict")
-def predict(data: request_body):
-    test_data = np.array([[ 
+async def predict(data: request_body):
+    input_data = [
         data.sepal_length,
         data.sepal_width,
         data.petal_length,
         data.petal_width
-    ]])
-    
-    class_idx = clf.predict(test_data)[0]
-    return {"class": target_names[class_idx]}
-
-
+    ]
+    prediction = await iris_model_actor.predict.remote(input_data) # type: ignore
+    return {"class": prediction}
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 @app.get("/")
 def serve_index():
